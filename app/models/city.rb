746c694e4652ac5ad63fd14ceb22cb8c082e9ef6
Greg Smith <greg_smith1992@hotmail.com>
2019-05-24 18:16:47 +0100

    Added cards css and html for index of cities

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,6 +2,8 @@ class City < ApplicationRecord
   has_many :user
   has_many :event
   validates :name, presence: true
-  validates :description, presence: true
-  validates :location, presence: true
+  validates :country_name, presence: true
+  mount_uploader :card_photo, PhotoUploader
+  mount_uploader :cover_photo, PhotoUploader
+
 end
