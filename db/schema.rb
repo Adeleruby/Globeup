7bfab92a88e1f3859322986547f5a7c4497de2a9
Julia Pissarro <juliapissarro@gmail.com>
2019-05-28 16:13:14 +0100

    removed the extra start and end time from events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_111927) do
+ActiveRecord::Schema.define(version: 2019_05_28_151005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,6 @@ ActiveRecord::Schema.define(version: 2019_05_28_111927) do
     t.datetime "ends"
     t.string "category"
     t.string "photo"
-    t.datetime "start"
-    t.datetime "end"
     t.index ["city_id"], name: "index_events_on_city_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
