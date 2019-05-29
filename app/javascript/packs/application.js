fe845cac9b9da7f060018a948518ec20d7ef354d
Greg Smith <greg_smith1992@hotmail.com>
2019-05-29 12:35:20 +0100

    Added search bar to the index page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 import 'select2/dist/css/select2.css';
 import { initSelect2 } from './components/init_select2';
+import { search } from './components/search';
 
 initSelect2();
+search();
---
+++ app/javascript/packs/components/search.js
@@ -0,0 +1,17 @@
+
+const search = () => {
+  $('.uis-form-icon').on('click', function(){
+    $(this).addClass('uis-active');
+
+    $(this).siblings('input').addClass('uis-active');
+    $(this).siblings('input').focus();
+
+    $(this).siblings('input').focusout(function(e){
+      $(this).removeClass('uis-active');
+      $(this).siblings('i').removeClass('uis-active');
+      $(this).val('');
+    })
+  })
+};
+
+export { search };
