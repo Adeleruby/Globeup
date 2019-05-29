6154b049818d80d28c5b2d4b3446819f24aa335f
Ayodele Ogunfadebo <a_ogunfadebo@yahoo.co.uk>
2019-05-29 18:00:36 +0100

    newhome with slider

---
+++ app/javascript/packs/components/slick.js
@@ -0,0 +1,14 @@
+import 'slick-carousel';
+
+$('.slick-carousel').slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  prevArrow: "<i class='l-arr fas fa-chevron-left'></i>",
+  nextArrow: "<i class='r-arr fas fa-chevron-right'></i>",
+  centerPadding: '10px',
+  // variableWidth: true,
+  autoplay: true,
+  speed: 500,
+  autoplaySpeed: 4000
+})
