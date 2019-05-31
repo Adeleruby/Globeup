7a092ac05978a07a938ce7c2ce421ed00865e152
Julia Pissarro <juliapissarro@gmail.com>
2019-05-31 16:32:21 +0100

    better styling on city guide page

--- app/controllers/city_guides_controller.rb
+++ app/controllers/city_guides_controller.rb
@@ -12,7 +12,7 @@ class CityGuidesController < ApplicationController
     html_file = open(url).read
     html_doc = Nokogiri::HTML(html_file)
 
-    html_doc.search('.entry-content p').each do |e|
+    html_doc.search('.entry-content p')[0...8].each do |e|
     @descriptions << e.text.strip
     end
   end
