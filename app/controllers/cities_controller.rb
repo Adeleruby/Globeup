262a4c0ac0f46dfa9460e19f5e455576c6962293
Greg Smith <greg_smith1992@hotmail.com>
2019-06-01 14:34:45 +0100

    Added side scrolling for members in a city show

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -7,7 +7,7 @@ class CitiesController < ApplicationController
   def show
     @city = City.find(params[:id])
     @display_events = Event.where(city: @city).sample(5)
-    @display_users = User.where(current_location: @city).sample(10)
+    @display_users = User.where(current_location: @city).sample(25)
     @user_interests = UserInterest.all
     @trending_interests = Interest.all.sample(5)
   end
