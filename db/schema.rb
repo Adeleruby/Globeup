9df31e1e564062279533b6b874d0dc97196d3228
Greg Smith <greg_smith1992@hotmail.com>
2019-06-04 10:36:35 +0100

    Changed text input to string so form submits on enter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_102306) do
+ActiveRecord::Schema.define(version: 2019_06_04_093227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 2019_06_03_102306) do
   create_table "messages", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "chat_room_id"
-    t.text "content"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["chat_room_id"], name: "index_messages_on_chat_room_id"
