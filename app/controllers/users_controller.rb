002639fd768a29a6e1963a141a2b0799951d8f79
Greg Smith <greg_smith1992@hotmail.com>
2019-06-05 22:46:29 +0100

    Added logic to account for users with no interests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,22 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    unless current_user == @user
+    if current_user != @user
       interest_ids = current_user.user_interests.map { |interest| interest.interest_id }
       interest_ids2 = @user.user_interests.map { |interest| interest.interest_id }
-      not_common = (interest_ids - interest_ids2) + (interest_ids2 - interest_ids)
-      unless not_common.empty?
+      if (interest_ids.nil? || interest_ids2.nil?)
+        not_common = []
+      else
+        not_common = (interest_ids - interest_ids2) + (interest_ids2 - interest_ids)
+      end
+      if !not_common.empty?
         common = interest_ids - not_common
+
         @common_interests = common.map { |interest_id| Interest.find(interest_id)}
         @not_common_interests = not_common.map { |interest_id| Interest.find(interest_id)}
+      else
+        @common_interests = []
+        @not_common_interests = []
       end
     end
   end
