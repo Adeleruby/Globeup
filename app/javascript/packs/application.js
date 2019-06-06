53b20c5f1dca0e02ce41c3ee3c469bc5fc3fcab4
Ayodele Ogunfadebo <a_ogunfadebo@yahoo.co.uk>
2019-06-06 22:29:07 +0100

    removed line 8

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@ import { search } from './components/search';
 import { eventListen } from './components/search';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from './components/init_mapbox';
-import { eventListen } from './components'
+
 
 initSelect2();
 
